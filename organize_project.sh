3afe1e1d520aecc8cc5d27f1881246a414b4f769
Matt Ring <mar420@georgetown.edu>
2021-09-28 22:42:38 -0400

    Initial commit

---
+++ organize_project.sh
@@ -0,0 +1,13 @@
+# Creates files
+
+mkdir scripts data output project_management
+
+mkdir scripts/wrangling scripts/analysis
+
+mkdir output/markdown output/plots
+
+mkdir data/raw data/processed
+
+mkdir data/raw/spatial data/processed/spatial
+
+mkdir project_management/1_collection project_management/2_storage project_management/3_preprocessing project_management/4_exploration project_management/5_analysis project_management/6_communicate
